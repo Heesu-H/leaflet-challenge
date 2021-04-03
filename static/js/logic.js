eb157c61fa1d8b7dabced5171f6e28286134af8b
Heesu-H <heesu456@hotmail.com>
2021-04-03 23:31:39 +1100

    added legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -26,18 +26,19 @@ d3.json(url, data => {
     var array = data.features
     var circleMarkers = [];
     console.log(data)
+
     array.forEach(object => {
 
         var magnitude = object.properties.mag
-        if (magnitude >= 5){
+        if (magnitude > 5){
             var colour = 'red'
-        } else if (magnitude >= 4) {
+        } else if (magnitude > 4) {
             colour = '#ff6700'
-        } else if (magnitude >= 3) {
+        } else if (magnitude > 3) {
             colour = '#ffa700'
-        } else if (magnitude >= 2) {
+        } else if (magnitude > 2) {
             colour = '#fff400'
-        } else if (magnitude >= 1) {
+        } else if (magnitude > 1) {
             colour = '#d6ff00'
         } else {
             colour = '#00ff38'
@@ -45,6 +46,7 @@ d3.json(url, data => {
 
         var geoLocation = (object.geometry.coordinates.slice(0,2))
         var coordinates = [geoLocation[1],geoLocation[0]]
+
         circleMarkers.push( 
             L.circle(coordinates, {
                 fillColor: colour,
@@ -66,10 +68,39 @@ d3.json(url, data => {
 
     //creating map
     var myMap = L.map('map', {
-        center: [0,0],
-    zoom: 3,
-    layers: [light, magnitudeLayer]
+        center: [0,15],
+        zoom: 3,
+        layers: [light, magnitudeLayer]
     })
+
+    //colours for legend
+    function getColor(d) {
+        return d > 5  ? 'red' :
+               d > 4  ? '#ff6700' :
+               d > 3  ? '#ffa700' :
+               d > 2  ? '#fff400' :
+               d > 1  ? '#d6ff00' :
+               d > 0  ? '#00ff38' :
+                        '#FFEDA0';
+    }
+    // adding legend
+    var legend = L.control({position:'bottomright'});
+    legend.onAdd = function(myMap) {
+        var div = L.DomUtil.create('div','info legend'),
+        grades = [0,1,2,3,4,5],
+        labels= [];
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    }
+
+    legend.addTo(myMap);
 
     //adding layer control panel
     L.control.layers(baseMaps, overlayMaps).addTo(myMap)
